db6d0b4436080c4b15f2eb5394f27ebcd7b69292
srivarshini gannavaram <srivarshinigannavaram@gmail.com>
2026-07-07 22:03:57 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='holiday_management',
+    version='0.1.0',
+    author="Gannavaram Srivarshini",
+    author_email="srivarshinigannavaram.ds@gmail.com",
+    packages=find_packages()
+)
